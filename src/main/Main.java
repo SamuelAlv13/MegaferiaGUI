3091df9a92d9a63e9a74d7d6f675eb6473d82733
SamuelAlv13 <amashta@uninorte.edu.co>
2025-11-16 17:14:35 -0500

    View y main agregado

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import view.MegaferiaFrame;
+
+/**
+ *
+ * @author Samuel Alvarado
+ */
+public class Main {
+    public static void main(String[] args) {
+        MegaferiaFrame Frame= new MegaferiaFrame();
+        Frame.setVisible(true);
+    }
+}
